1a035b85d9417ba9cfd5a35ac44a6293a200f191
MarinaStoneQa <marinastoneqa@gmail.com>
2021-06-16 14:37:24 +0300

    Rabotay. tvar

---
+++ ReactJS/unit_1/src/App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import './App.css';
+let text = 'test variable';
+const style = {
+  fontSize: '24px',
+  fontStyle: 'italic',
+  color: 'red'
+}
+function App() {
+  let text2 = '222';
+  return (
+    <>
+      <div className="container">
+        <h1 style={style}>app_1</h1>
+        <img src="./images/apple.jpeg" alt="apple" />
+        <ul>
+          <li>Hello</li>
+          <li>{text + text2}</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
