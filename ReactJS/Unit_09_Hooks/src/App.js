e639aa28924dbff7cfcd104c62dfaf76a1dd7682
MarinaStoneQa <marinastoneqa@gmail.com>
2021-07-15 21:01:52 +0300

    2 task

--- ReactJS/Unit_09_Hooks/src/App.js
+++ ReactJS/Unit_09_Hooks/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import PlaceholderPostHook from './PlaceholderPostHook'
-import CommentHook from './ComponentHook'
+import CommentHook from './CommentHook'
 
 function App() {
   return (
     <>
       <PlaceholderPostHook />
+      <CommentHook />
     </>
   );
 }
