714e5608a3e2b487127a3083d4bfb36cd95c2df4
MarinaStoneQa <marinastoneqa@gmail.com>
2021-07-23 17:49:37 +0300

    -

---
+++ ReactJS/unit_12/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import GoodsList from './containers/GoodsList';
+
+function App() {
+  return (
+    <>
+      <div>
+        {/* {Вывод даных и зхранилища} */}
+        <GoodsList />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJS/unit_12/src/containers/GoodsList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectGoods } from '../store/goodsSlice';
+
+import Goods from '../components/Goods'
+
+// берем даные из хранилища list data
+
+function GoodsList() {
+
+    const goods = useSelector(selectGoods);
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <div className='goods=field'>
+                {goods.map(item => <Goods title={item.title} cost={item.cost} image={item.image} articul={item.articul} key={item.articul} />)}
+            </div>
+        </>
+    )
+}
+export default GoodsList
