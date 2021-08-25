64c898b53f9f878f49fef70b107efb01af6abdca
MarinaStoneQa <marinastoneqa@gmail.com>
2021-08-25 10:26:01 +0300

    info section

--- PracticeSite/Cosmic/js/main.js
+++ PracticeSite/Cosmic/js/main.js
@@ -1,3 +1,6 @@
-$(function(){
-
+$(function () {
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow_left.png" alt="left">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow_right.png" alt="right">',
+    });
 });
