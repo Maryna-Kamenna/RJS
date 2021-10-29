7a5d65980dac719f0136a2c0e1f995ac0a172b90
MarinaStoneQa <marinastoneqa@gmail.com>
2021-10-29 13:03:47 +0300

    end

--- PracticeSite/Safari/js/main.js
+++ PracticeSite/Safari/js/main.js
@@ -28,4 +28,21 @@ $(function(){
 	$('.menu__btn').on('click', function(){
 		$('.menu__list').toggleClass('menu__list--active')
 		});
+
+		$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
 });
