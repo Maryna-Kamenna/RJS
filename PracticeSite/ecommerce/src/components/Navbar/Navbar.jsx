9dd0ddaa70b07a2ae8981aa34d9e66b13c986795
MarinaStoneQa <marinastoneqa@gmail.com>
2021-11-18 16:32:09 +0200

    day2

--- PracticeSite/ecommerce/src/components/Navbar/Navbar.jsx
+++ PracticeSite/ecommerce/src/components/Navbar/Navbar.jsx
@@ -9,15 +9,18 @@ import {
 } from "@material-ui/core";
 import logo from "../../assets/logo.png";
 import useStyles from "./styles";
+import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
+  const location = useLocation();
+
 
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
+          <Typography  component={Link} to="/" variant="h6" className={classes.title} color="inherit">
             <img
               src={logo}
               alt="commerce.js"
@@ -27,12 +30,14 @@ const Navbar = () => {
             Commerce.js
           </Typography>
           <div className={classes.grow} />
-          <div className={classes.button} />
-          <IconButton area-label="Show cart items" color="inherit">
-            <Badge badgeContent={2} color="secondary">
+          { location.pathname==='/' && (
+          <div className={classes.button}>
+          <IconButton component={Link} to="/cart" area-label="Show cart items" color="inherit">
+            <Badge badgeContent={totalItems} color="secondary">
               <ShoppingCart />
             </Badge>
           </IconButton>
+          </div> )}
         </Toolbar>
       </AppBar>
     </>
